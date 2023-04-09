42fca70a4577d5e11f374a0001c9efc31008f80f
이문빈 <3412mb@gmail.com>
2023-04-09 23:24:51 +0900

    Git Hooks test

---
+++ inventory/InventoryDebug.lua
@@ -0,0 +1,43 @@
+--Properties--
+
+Entity reset 
+Entity getElixir 
+Entity getPowerElixir 
+Entity getSword 
+
+
+--Methods--
+
+[Client Only]
+void OnBeginPlay()
+{
+	self.reset:ConnectEvent("ButtonClickEvent", self.Reset)
+	self.getElixir:ConnectEvent(ButtonClickEvent, function() self:Get("2") end)
+	self.getPowerElixir:ConnectEvent(ButtonClickEvent, function() self:Get("3") end)
+	self.getSword:ConnectEvent(ButtonClickEvent, function() self:GetEquip("10002") end)
+}
+
+[Default]
+void Reset()
+{
+	local id = _UserService.LocalPlayer.Name
+	_InventoryServer:ResetInventory(id)
+}
+
+[Default]
+void Get(string code)
+{
+	local id = _UserService.LocalPlayer.Name
+	_InventoryServer:AddItem(id, code)
+}
+
+[Default]
+void GetEquip(string code)
+{
+	local id = _UserService.LocalPlayer.Name
+	_InventoryServer:AddEquip(id, code)
+}
+
+
+--Events--
+
---
+++ inventory/ItemSlot.lua
@@ -0,0 +1,20 @@
+--Properties--
+
+string cnt = "Text_item_count"
+string line = "line_selected"
+
+
+--Methods--
+
+[Client Only]
+void OnBeginPlay()
+{
+	local cnt = self.Entity:GetChildByName(self.cnt)
+	local line = self.Entity:GetChildByName(self.line)
+	cnt.Enable = false
+	line.Enable = false
+}
+
+
+--Events--
+
