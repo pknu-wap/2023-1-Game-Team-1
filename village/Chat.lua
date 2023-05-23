32ba4c21929a53667ef901aa3780de0f5c9b8485
이문빈 <3412mb@gmail.com>
2023-05-23 16:20:56 +0900

    대화 기능 추가

--- village/Chat.lua
+++ village/Chat.lua
@@ -1,6 +1,18 @@
 --Properties--
 
 string dataSetName = ""
+table names
+table images
+table scripts
+number idx = 1
+Entity btnPrev
+Entity btnNext
+Component nameText
+Component imageSprite
+Component scriptText
+Entity ui
+Entity uiDialog
+Entity btnComplete
 
 
 --Methods--
@@ -11,13 +23,71 @@ void OnBeginPlay()
 	-- 데이터셋 불러오기
 	
 	local dataSet = _DataService:GetTable(self.dataSetName)
-	for _, row in pairs(dataSet:GetAllRow()) do
-		local name = row:GetItem("name")
-		local image = row:GetItem("image")
-		local script = row:GetItem("script")
+	for k, row in pairs(dataSet:GetAllRow()) do
+		self.names[k] = row:GetItem("name")
+		self.images[k] = row:GetItem("image")
+		self.scripts[k] = row:GetItem("script")
 	end
+	
+	-- 이벤트 연결
+	
+	self.ui.Enable = true
+	self.uiDialog.Enable = true
+	self.btnPrev:ConnectEvent(ButtonClickEvent, self.OnClickBtnPrev)
+	self.btnNext:ConnectEvent(ButtonClickEvent, self.OnClickBtnNext)
+	self.btnComplete:ConnectEvent(ButtonClickEvent, self.OnClickBtnComplete)
+}
+
+[Client]
+void UpdateUI(number idx)
+{
+	self.nameText.Text = self.names[idx]
+	self.imageSprite.ImageRUID = self.images[idx]
+	self.scriptText.Text = self.scripts[idx]
+	
+	self.btnPrev.Enable = (idx ~= 1)
+	self.btnNext.Enable = (idx ~= #self.scripts)
+}
+
+[Client]
+void OnClickBtnPrev()
+{
+	if self.idx == 1 then return end
+	self.idx = self.idx - 1
+	self:UpdateUI(self.idx)
+}
+
+[Client]
+void OnClickBtnNext()
+{
+	if self.idx == #self.scripts then return end
+	self.idx = self.idx + 1
+	self:UpdateUI(self.idx)
+}
+
+[Client]
+void OnClickBtnComplete()
+{
+	self.uiDialog.Enable = false
 }
 
 
 --Events--
+
+[Default]
+HandleKeyDownEvent(KeyDownEvent event)
+{
+	--------------- Native Event Sender Info ----------------
+	-- Sender: InputService
+	-- Space: Client
+	---------------------------------------------------------
+	
+	-- Parameters
+	local key = event.key
+	---------------------------------------------------------
+	if key == KeyboardKey.Q then
+		self.uiDialog.Visible = true
+		self:UpdateUI(1)
+	end
+}
 
